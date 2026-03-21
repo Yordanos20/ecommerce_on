47814f99a8eea2c6e9236d60d975edac86300950
yordanos20 <segudeyordanos@gmail.com>
2026-03-22 00:16:27 +0300

    Fix API URL to connect to Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "/api";
+export const BASE_URL = "https://ecommerce-backend-ol0h.onrender.com/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
